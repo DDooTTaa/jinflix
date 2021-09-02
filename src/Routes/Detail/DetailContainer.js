e87b4edb7df520a6bdf3e989eddada27b8c2cd98
KongJinYong <dimeon@naver.com>
2021-09-02 19:28:05 +0900

    container finish

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,16 +1,51 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
-
+import { moviesApi, tvApi } from "../../api.js";
 
 export default class extends React.Component {
-    state = {
-        result: null,
-        error: null,
-        loading: true
-    };
+    constructor(props) {
+        super(props);
+        const { location: { pathname } } = props;
+        this.state = {
+            result: null,
+            error: null,
+            loading: true,
+            isMovie: pathname.includes("/movie")
+        };
+    }
 
+    async componentDidMount() {
+        const { match: {
+            params: { id }
+        },
+            history: { push },
+            location: { pathname }
+        } = this.props;
+        const { isMovie } = this.state;
+        const parseId = parseInt(id);
+        if (isNaN(parseId)) {
+            return push("/");
+        }
+        let result = null;
+        try {
+            if (isMovie) {
+                const request = await moviesApi.movieDetail(parseId);
+                result = request.data;
+            } else {
+                const request = await tvApi.showDetail(parseId);
+                result = request.data;
+            }
+
+        } catch {
+            this.setState({ error: "can't find" })
+        } finally {
+            this.setState({ loading: false, result });
+        }
+    }
     render() {
         const { result, error, loading } = this.state;
+        console.log(this.state);
+        console.log(result);
         return <DetailPresenter
             result={result}
             error={error}
