141bd5ea915d4e1ca801668265ed03950504152a
KongJinYong <dimeon@naver.com>
2021-10-07 18:34:59 +0900

    함수형 시작

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,15 +2,23 @@ import React, { Component } from "react";
 import GlobalStyles from "./GlobalStyles";
 import Router from "./Router";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Router />
-        <GlobalStyles />
-      </>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//         <Router />
+//         <GlobalStyles />
+//       </>
+//     );
+//   }
+//}
+const App = () => {
+  return (
+    <>
+      <Router />
+      <GlobalStyles />
+    </>
+  )
 }
 
 export default App;
